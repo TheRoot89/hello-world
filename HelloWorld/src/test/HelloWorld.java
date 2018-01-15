d139155e3abf38a7a221dcec6dc9ec5a205e9431
TheRoot89 <toni.babik@gmx.de>
2018-01-15 09:07:36 +0100

    added hello world and ant build file

---
+++ HelloWorld/src/test/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Toni
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
